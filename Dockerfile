c3e04bb506af4fe8fb0e38784ac846addc0da30a
MiguelTiradoGuzman <migueltiradoguzman@gmail.com>
2024-01-25 17:43:56 +0100

    Aniado instalación de TA-Lib

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,17 @@ FROM python:3.12-rc-slim
 
 RUN apt-get update && apt-get install -y build-essential
 
+RUN apt-get update && apt-get install -y curl
+
+RUN curl -L https://sourceforge.net/projects/ta-lib/files/ta-lib/0.4.0/ta-lib-0.4.0-src.tar.gz/download?use_mirror=deac-ams -o ta-lib.tar.gz \
+    && tar -xvzf ta-lib.tar.gz \
+    && cd ta-lib \
+    && ./configure --prefix=/usr \
+    && make \
+    && make install \
+    && cd .. \
+    && rm -rf /app/ta-lib /app/ta-lib.tar.gz
+
 RUN adduser --disabled-password -u 1001 pythonuser
 
 USER pythonuser
@@ -11,3 +22,6 @@ WORKDIR /app
 COPY requirements.txt .
 
 RUN pip install --upgrade pip; pip install -r requirements.txt
+
+RUN pip install TA-Lib
+
--- src/algotrading.py
+++ src/algotrading.py
@@ -3,26 +3,30 @@ import json
 import pandas as pd
 from alpaca_trade_api import REST, TimeFrame
 from datetime import datetime, timedelta
+import os
+import talib
 
-# Loading the environment variables
-api_url = os.environ.get("API_URL")
-api_key = os.environ.get("API_KEY")
-api_secret = os.environ.get("API_SECRET")
+api_key = os.environ['API_KEY']
+secret_key = os.environ['SECRET_KEY']
+base_url = os.environ['API_URL']
 
-symbols = ["MMM"]
+symbols = ['MMM']
 
-api = REST(key_id=api_key, secret_key=api_secret, base_url=api_url, api_version="v2")
+api = REST(key_id=api_key, secret_key=secret_key, base_url=base_url, api_version='v2')
 # Setting the date range of stock prices we want to get
 end_date = datetime.today() - timedelta(days=1)
 start_date = end_date - timedelta(days=365 * 2)
 
-df_barset = api.get_bars(
-    symbols,
-    TimeFrame.Day,
-    start_date.strftime("%Y-%m-%d"),
-    end_date.strftime("%Y-%m-%d"),
-    adjustment="all",
-).df
+df_barset = api.get_bars(symbols, TimeFrame.Day, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'), adjustment='all').df
 df_barset = df_barset.reset_index(level=0)
 
 print(df_barset.head())
+
+ma_10 = talib.MA(df_barset['close'], timeperiod=10, matype=0)
+
+ma_30  = talib.MA(df_barset['close'], timeperiod=30, matype=0)
+
+print(ma_10)
+
+print(ma_30)
+
