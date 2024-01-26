781c03efc247fd900eacde5cd651ae295ce90c5c
MiguelTiradoGuzman <migueltiradoguzman@gmail.com>
2024-01-26 10:24:29 +0100

    Añado libreria de backtesting y hago primera prueba

--- src/algotrading.py
+++ src/algotrading.py
@@ -5,6 +5,13 @@ from alpaca_trade_api import REST, TimeFrame
 from datetime import datetime, timedelta
 import os
 import talib
+import numpy as np
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+
+from backtesting.test import SMA, GOOG
+
+# https://kernc.github.io/backtesting.py/
 
 api_key = os.environ["API_KEY"]
 secret_key = os.environ["SECRET_KEY"]
@@ -26,12 +33,57 @@ df_barset = api.get_bars(
 ).df
 df_barset = df_barset.reset_index(level=0)
 
-print(df_barset.head())
+# print(df_barset)
 
 ma_10 = talib.MA(df_barset["close"], timeperiod=10, matype=0)
 
 ma_30 = talib.MA(df_barset["close"], timeperiod=30, matype=0)
 
-print(ma_10)
+# print(ma_10)
 
-print(ma_30)
+# print(ma_30)
+
+matriz_estrategia = np.vstack((df_barset["close"], ma_10, ma_30))
+
+# Eliminar las filas que contienen NaN
+matriz_estrategia = matriz_estrategia[~np.isnan(matriz_estrategia).any(axis=1)]
+
+# Nombres de las columnas
+nombres_columnas = ["cierre", "ma10", "ma30"]
+
+# Crear un dtype con nombres para las columnas
+dtype = np.dtype({"names": nombres_columnas, "formats": [float, float, float]})
+
+# Crear una nueva matriz con dtype
+matriz_estrategia = np.array(matriz_estrategia, dtype=dtype)
+
+
+class SmaCross(Strategy):
+    n1 = 10
+    n2 = 20
+
+    def init(self):
+        close = self.data.Close
+        self.sma1 = self.I(SMA, close, self.n1)
+        self.sma2 = self.I(SMA, close, self.n2)
+
+    def next(self):
+        if crossover(self.sma1, self.sma2):
+            self.buy()
+        elif crossover(self.sma2, self.sma1):
+            self.sell()
+
+df_barset = df_barset.rename(columns={'open': 'Open', 'close': 'Close', 'high': 'High', 'low': 'Low', 'volume': 'Volume'})
+df_barset['timestamp'] = pd.to_datetime(df_barset['timestamp'], errors='coerce')
+
+df_barset = df_barset.set_index('timestamp')
+
+# print(df_barset.columns)
+
+bt = Backtest(df_barset, SmaCross, cash=10000, commission=0.002, exclusive_orders=True)
+
+output = bt.run()
+
+print(output)
+
+# print(output._strategy)
