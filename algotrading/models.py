3826a3b8dcdf55d4988e38f47bd49db0d9123d28
Pablo Luque Salguero <pablols114@gmail.com>
2024-02-14 13:27:16 +0100

    Creado modelo para validar resultados de estrategia, #11
    Co-authored-by: MiguelTiradoGuzman <MiguelTiradoGuzman@users.noreply.github.com>

--- algotrading/models.py
+++ algotrading/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 
@@ -6,3 +7,13 @@ class Asset(BaseModel):
     exchange: str
     symbol: str
     name: str
+
+
+class BacktestResults(BaseModel):
+    start_time: datetime
+    end_time: datetime
+    duration: str
+    exposure_time: float
+    equity_final: float
+    equity_peak: float
+    ret: float
