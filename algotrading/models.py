762865730d1c4e3079a6e84fe6b181496ea14f3a
Pablo Luque Salguero <pablols114@gmail.com>
2024-02-16 12:05:09 +0100

    Field de Pydantic añade campos informativos, #13

--- algotrading/models.py
+++ algotrading/models.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Asset(BaseModel):
@@ -10,10 +9,10 @@ class Asset(BaseModel):
 
 
 class BacktestResults(BaseModel):
-    start_time: datetime
-    end_time: datetime
-    duration: str
-    exposure_time: float
-    equity_final: float
-    equity_peak: float
-    ret: float
+    start_time: str = Field(title="Start Time", description="")
+    end_time: str = Field(title="End Time", description="")
+    duration: str = Field(title="Duration", description="")
+    exposure_time: float = Field(title="Exposure Time", description="")
+    equity_final: float = Field(title="Final Equity [$]", description="")
+    equity_peak: float = Field(title="Peak Equity [$]", description="")
+    ret: float = Field(title="Return [%]", description="")
